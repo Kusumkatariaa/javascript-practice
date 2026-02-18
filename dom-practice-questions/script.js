430ae41be58364c7afa71e7ca6fdd78f2890138f
Kusumkatariaa <kusumkataria899@gmail.com>
2026-02-18 18:57:29 +0530

    4 questions solved 18th feb

--- dom-practice-questions/script.js
+++ dom-practice-questions/script.js
@@ -268,3 +268,232 @@ goBtn.addEventListener('click', function () {
     turnOff();
     goCircle.style.background = 'green';
 });
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// # Intermediate Level DOM + Math Practice Tasks
+
+
+// ## 1. Dice Game
+
+// Create two dice images dynamically using `document.createElement()`. Use
+// `Math.random()` to generate numbers (1--6). Display both dice results
+// and show the winner based on the higher number. Render everything
+// dynamically using `append()` or `appendChild()`.
+
+let rollDiceBtn = document.querySelector(".rollDice");
+let gameBox = document.querySelector(".game-container");
+
+let diceImages = ["https://upload.wikimedia.org/wikipedia/commons/c/c5/Dice-1.png",
+    "https://upload.wikimedia.org/wikipedia/commons/1/18/Dice-2.png",
+    "https://upload.wikimedia.org/wikipedia/commons/7/70/Dice-3.png",
+    "https://upload.wikimedia.org/wikipedia/commons/a/a9/Dice-4.png",
+    "https://upload.wikimedia.org/wikipedia/commons/6/6c/Dice-5.png",
+    "https://upload.wikimedia.org/wikipedia/commons/5/5c/Dice-6.png"]
+
+
+
+rollDiceBtn.addEventListener('click', function () {
+    gameBox.innerHTML = "";
+
+    let randomNum1 = Math.floor(Math.random() * diceImages.length);
+    let randomNum2 = Math.floor(Math.random() * diceImages.length);
+    console.log(randomNum1, randomNum2);
+
+    let dice1 = document.createElement("img");
+    dice1.src = diceImages[randomNum1];
+
+    let dice2 = document.createElement("img");
+    dice2.src = diceImages[randomNum2];
+    gameBox.appendChild(dice1);
+    gameBox.appendChild(dice2);
+
+    let winnerText = document.createElement('div');
+    winnerText.setAttribute('class', "win-text");
+
+    gameBox.appendChild(winnerText);
+
+    if (randomNum1 > randomNum2) {
+        winnerText.textContent = "Player 1 Wins"
+    } else if (randomNum2 > randomNum1) {
+        winnerText.textContent = "Player 2 Wins"
+    } else {
+        winnerText.textContent = "Match Draw"
+    }
+
+
+});
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## 2. Random Color Card Generator
+
+// Create a card on button click. Generate a random RGB background color
+// using `Math.floor()` and `Math.random()`. Set a random width and height.
+// Assign a unique `data-id` using `setAttribute()`.
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## 3. Lottery System
+
+// Take user input (1--50). Generate a random number using `Math.ceil()`.
+// If the number matches user input, display "You Won" otherwise "Try
+// Again". Update result dynamically in the DOM.
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## 4. Dynamic Image Gallery
+
+// Store image URLs in an array. On button click, pick a random image using
+// `Math.floor()`. Create an `<img>` element dynamically and append it to a
+// container. Use `getAttribute("src")` to print the image source in
+// console.
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## 5. Progress Bar Simulator
+
+// Create a progress bar dynamically. On button click, increase progress
+// randomly between 10--30%. Update the width style property dynamically.
+// Stop progress at 100%.
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## 6. Random Box Remover
+
+// Create 5 boxes dynamically. Assign each box a unique `data-id` using
+// `setAttribute()`. On click of a box, remove only that specific box. Use
+// `getAttribute()` to identify which box was clicked.
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## 7. Mini Gambling UI
+
+// Generate 3 random numbers between 1--9. Display all numbers dynamically.
+// If all three numbers are the same, show "Jackpot". Otherwise display
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## IPL WINNER PREDICTION
+
+let teamLogo = document.querySelector(".logo");
+let teamName = document.querySelector(".team_name");
+let predictionBtn = document.querySelector(".prediction-btn");
+let mainBox2 = document.querySelector(".ipl-winner")
+
+let teams = [
+    {
+        name: "Royal Challengers Bengaluru", logo: "https://media.fortuneindia.com/fortune-india/2025-11-06/6incrk23/RCB.jpg?w=640&auto=format,compress&q=80"
+    },
+    {
+        name: "Chennai Super Kings", logo: "https://i.pinimg.com/736x/ea/40/fd/ea40fd13fa7d46618c5567146f07ba41.jpg"
+    },
+    {
+        name: "Kolkata Knight Riders", logo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Kolkata_Knight_Riders_Logo.svg/960px-Kolkata_Knight_Riders_Logo.svg.png"
+    },
+    {
+        name: "Punjab Kings", logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Punjab_Kings_Logo.svg/250px-Punjab_Kings_Logo.svg.png"
+    },
+    {
+        name: "Mumbai Indians", logo: "https://i.pinimg.com/736x/e8/87/a8/e887a81959a66337b7ccc7835c38470e.jpg"
+    }
+]
+mainBox2.style.display = "none";
+predictionBtn.addEventListener('click', function () {
+    mainBox2.style.display = "block";
+    let randomTeam = Math.floor(Math.random() * teams.length);
+    console.log(randomTeam, teams[randomTeam])
+    teamName.textContent = teams[randomTeam].name;
+    teamLogo.src = teams[randomTeam].logo;
+});
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ## Showing random images on button click 
+
+let imgArr = ["https://i.pinimg.com/736x/38/33/90/3833900cee9e69c78a98e5d9e0ff0988.jpg",
+    "https://i.pinimg.com/736x/a5/ad/20/a5ad200ca202b684bfdbe7a65294743f.jpg",
+    "https://i.pinimg.com/736x/dd/88/75/dd8875593d43d76167365052e6dfd879.jpg",
+    "https://i.pinimg.com/1200x/0e/39/1f/0e391f1b15dfd12d94ba08a0dbfa400b.jpg",
+    "https://i.pinimg.com/736x/1a/3b/ac/1a3bac79d633d391c50d085d4b6edf2d.jpg"]
+
+let clickButtton = document.querySelector(".click-me");
+let imageBox = document.querySelector(".img_box img")
+
+clickButtton.addEventListener('click', function () {
+    let randomIndex = Math.floor(Math.random() * imgArr.length);
+    imageBox.src = imgArr[randomIndex];
+    imageBox.style.width = "250px";
+    imageBox.style.height = "250px";
+    imageBox.style.objectFit = "cover";
+    imageBox.style.objectPosition = "center";
+
+});
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
